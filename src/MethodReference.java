8f00f38a39a21e7ce0571becffdd0b6a72c263e6
bibhuti-agarwal <bibhuti.agarwal2015@gmail.com>
2018-04-30 00:21:14 +0530

    example-3 (method references)

---
+++ src/MethodReference.java
@@ -0,0 +1,43 @@
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Method references
+ * 
+ * @author Bibhuti
+ * @version 1.0
+ */
+public class MethodReference {
+
+	public static void main(String[] args) {
+
+		// using lambda expression
+		Stream.of(1,2,3,4,5).forEach(x->System.out.println(x));
+
+		System.out.println("-----------------------");
+		// using method references to access println
+		Stream.of(1,2,3,4,5).forEach(System.out::println);
+		
+		System.out.println("-----------------------");
+		// assigning the method reference to functional interface
+		Consumer<Integer> printer = System.out::println;
+		Stream.of(1,2,3,4,5).forEach(printer);
+		
+		System.out.println("----------------------");
+		// method reference to a static method.
+		Supplier<Double> supplier = Math::random;
+		Consumer<Double> consumer = System.out::println;
+		Stream.generate(supplier).limit(5).forEach(consumer);
+		
+		// NOTE: supplier is a functional interface whose single abstract 
+		// method takes no argument and produces single result and reverse 
+		// is the case with consumer.
+		
+		System.out.println("----------------------");
+		// method reference to a static method, optimized way.
+		Stream.generate(Math::random).limit(5).forEach(System.out::println);
+
+	}
+
+}
