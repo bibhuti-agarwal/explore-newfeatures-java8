0dca2cb26e1ec49c860c6f7f57cab5117baa47d9
bibhuti-agarwal <bibhuti.agarwal2015@gmail.com>
2018-04-29 15:31:59 +0530

    example 1 and resources

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,6 @@
 /**
  * simple hello world using Java 8 lambda expression. 
- */
-
-/**
+ * 
  * @author Bibhuti
  * @version 1.0
  */
@@ -12,6 +10,17 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		// Example-1 : assigning lambda expression to variable.
+		// Runnable is annotated as FunctionalInterface
+		Runnable r = () -> {
+			System.out.println("Hello World!");
+		};
+		
+		new Thread(r).start();
+		
+		// Example-2
+		// above example-1 can be optimized as below.
 		new Thread(() -> System.out.println("Hello World!")).start();
 	}
 
